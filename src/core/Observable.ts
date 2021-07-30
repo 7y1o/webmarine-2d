2dacceb52c535340b0ffc73817441f000f871d96
7y1o <tdesu.webdesign@gmail.com>
2021-07-30 21:51:41 +0300

    Finished Observable class

--- src/core/Observable.ts
+++ src/core/Observable.ts
@@ -1,4 +1,49 @@
 /** Observable base */
 export class Observable {
-    // TODO: observer
+    protected listeners;
+
+    /** Make observable */
+    public constructor() {
+        this.listeners = {};
+    }
+
+    /** Bind observer */
+    public on(event: string, callback: void): number {
+        if (!this.listeners[event])
+            this.listeners[event] = [];
+
+        this.listeners[event].push({ once: false, callback });
+        return this.listeners[event].length - 1;
+    }
+
+    /** Bind observer that will called once */
+    public once(event: string, callback: void): number {
+        if (!this.listeners[event])
+            this.listeners[event] = [];
+        
+        this.listeners[event].push({ once: true, callback });
+        return this.listeners[event].length - 1;
+    }
+
+    /** Unbind listener */
+    public off(event: string, id: number): void {
+        if (!this.listeners[event]?.[id])
+            return;
+        
+        this.listeners[event] = this.listeners[event].slice(id, 1);
+    }
+
+    /** Emit event to observers */
+    public emit(event: string, ...args: any[]) {
+        if (!this.listeners[event])
+            return;
+
+        for (const obId in this.listeners[event]) {
+            this.listeners[event][obId].callback();
+
+            if (this.listeners[event][obId].once) {
+                this.listeners[event] = this.listeners[event].slice(obId, 1);
+            }
+        }
+    }
 }

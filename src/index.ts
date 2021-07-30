d2a84c363476e8ff18b188bb997ca2a0ab353efe
7y1o <tdesu.webdesign@gmail.com>
2021-07-31 01:35:33 +0300

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 // Core
-export {Engine} from './core/Engine';
-export {Renderer} from './core/Renderer';
-export {Observable} from './core/Observable';
+export { Engine } from './core/Engine';
+export { Vector1 } from './core/Vector1';
+export { Vector2 } from './core/Vector2';
+export { Renderer } from './core/Renderer';
+export { Observable } from './core/Observable';
+
+// Renderer
+export { Camera } from './renderer/Camera';
+export { Filter } from './renderer/Filter';
+export { RenderTarget } from './renderer/RenderTarget';
+export { RendererObservable } from './renderer/RendererObservable';

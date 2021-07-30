f7525fec7ffb74d095ec3785d4a434c82e66565d
7y1o <tdesu.webdesign@gmail.com>
2021-07-30 18:11:04 +0300

    Added config, created constructor and makeFullWidth factory

--- src/core/Renderer.ts
+++ src/core/Renderer.ts
@@ -1,28 +1,68 @@
+import type {RendererConfig} from './refs/RendererConfig';
+
+/** Canvas renderer */
 export class Renderer {
 
-    /**
-     * @description Canvas holder
-     */
+    /** Canvas holder */
     protected canvas: HTMLCanvasElement;
 
-    /**
-     * @description 2D rendering context of canvas
-     */
+    /** 2D rendering context of canvas */
     protected ctx: CanvasRenderingContext2D;
 
-    /**
-     * @description Limit framerate
-     */
+    /** Limit framerate */
     protected fps: number | false;
 
-    /**
-     * @description 
-     */
+    /** Construct renderer and get canvas with canvas context */
+    public constructor(config?: RendererConfig) {
+        if (!config) config = {};
+        if (!config.id || !document.getElementById(config.id) || !(document.getElementById(config.id) instanceof HTMLCanvasElement)) {
+            this.canvas = document.createElement('canvas');
+            document.body.appendChild(this.canvas);
+        } else {
+            this.canvas = <HTMLCanvasElement>document.getElementById(config.id);
+        }
 
-    constructor(id?: string) {
-        if (id) 
-            this.canvas = <HTMLCanvasElement>document.getElementById(id);
+        this.ctx = this.canvas.getContext('2d');
+        this.fps = config.fps ?? false;
+        
+        this.canvas.width = config.width || config.adjust ? this.canvas.parentElement.offsetWidth : 800;
+        this.canvas.width = config.height || config.adjust ? this.canvas.parentElement.offsetHeight : 600;
+        this.ctx.imageSmoothingEnabled = config.antialias?.enabled ?? true;
+        this.ctx.imageSmoothingQuality = config.antialias?.enabled ? config.antialias?.quality ?? 'medium' : 'medium';
+        this.canvas.style.cssText = config.style ?? `
+            width: ${config.adjust ? '100%' : (config.width ?? 800) + 'px' };
+            height: ${config.adjust ? '100%' : (config.height ?? 800) + 'px' };
+        `;
 
-        this.canvas = this.canvas ?? <HTMLCanvasElement>document.createElement('canvas');
+        if (config.resizeTrack) {
+            window.addEventListener('resize', () => {
+                if (config.dependsElement) {
+                    this.canvas.width = config.dependsElement instanceof Window ? config.dependsElement.innerWidth : config.dependsElement.offsetWidth;
+                    this.canvas.height = config.dependsElement instanceof Window ? config.dependsElement.innerHeight : config.dependsElement.offsetHeight;
+                } else {
+                    this.canvas.width = this.canvas.parentElement.offsetWidth;
+                    this.canvas.height = this.canvas.parentElement.offsetHeight;
+                }
+            });
+        }
+    }
+
+    /** Factory for full-width canvas */
+    public static makeFullWith(): Renderer {
+        return new Renderer({
+            width: window.innerWidth,
+            height: window.innerHeight,
+            dependsElement: window,
+            resizeTrack: true,
+            style: `
+                position: fixed;
+                z-index: 10;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+            `,
+            fps: 60
+        });
     }
 }

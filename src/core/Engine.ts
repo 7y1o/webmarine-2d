4e33a5b88fdc9157bb7d555dc3dbc8e7f03b1854
7y1o <tdesu.webdesign@gmail.com>
2021-07-30 18:11:24 +0300

    Added render holder

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -1,1 +1,8 @@
-export class Engine {}
+import { Renderer } from "./Renderer";
+
+/** Entry point of game engine */
+export class Engine {
+
+    /** Renderer class holder */
+    protected render: Renderer;
+}

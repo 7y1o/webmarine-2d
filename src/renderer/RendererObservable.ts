dd7185a3801af442552a1f1dd4e9b3b85c228ec1
7y1o <tdesu.webdesign@gmail.com>
2021-07-31 02:21:39 +0300

    Removed "render" event

--- src/renderer/RendererObservable.ts
+++ src/renderer/RendererObservable.ts
@@ -2,8 +2,8 @@ import { Observable } from "../core/Observable";
 
 /** Renderer observable class */
 export abstract class RendererObservable extends Observable {
-    public abstract on(event: 'before_render' | 'render' | 'after_render', callback: void): number;
-    public abstract once(event: 'before_render' | 'render' | 'after_render', callback: void): number;
-    public abstract off(event: 'before_render' | 'render' | 'after_render', id: number): void;
-    public abstract emit(event: 'before_render' | 'render' | 'after_render', ...data: any[]): void;
+    public abstract on(event: 'before_render' | 'after_render', callback: void): number;
+    public abstract once(event: 'before_render' | 'after_render', callback: void): number;
+    public abstract off(event: 'before_render' | 'after_render', id: number): void;
+    public abstract emit(event: 'before_render' | 'after_render', ...data: any[]): void;
 }

7bfc1a29567f2aa93f52ac37ea3e5cfb3b998d68
7y1o <tdesu.webdesign@gmail.com>
2021-07-30 22:49:43 +0300

    Added comments in methods, observable getter and fps limit setter

--- src/core/Renderer.ts
+++ src/core/Renderer.ts
@@ -1,3 +1,4 @@
+import { RendererObservable } from '../renderer/RendererObservable';
 import type {RendererConfig} from './refs/RendererConfig';
 
 /** Canvas renderer */
@@ -9,8 +10,11 @@ export class Renderer {
     /** 2D rendering context of canvas */
     protected ctx: CanvasRenderingContext2D;
 
-    /** Limit framerate */
+    /** Limit fps */
     protected fps: number | false;
+
+    /** Event listeners */
+    protected readonly obersvable: RendererObservable;
 
     /** Construct renderer and get canvas with canvas context */
     public constructor(config?: RendererConfig) {
@@ -22,9 +26,11 @@ export class Renderer {
             this.canvas = <HTMLCanvasElement>document.getElementById(config.id);
         }
 
+        // Setup canvas rendering context and fps limit
         this.ctx = this.canvas.getContext('2d');
         this.fps = config.fps ?? false;
         
+        // Setup width, height, adjust and antialiasing
         this.canvas.width = config.width || config.adjust ? this.canvas.parentElement.offsetWidth : 800;
         this.canvas.width = config.height || config.adjust ? this.canvas.parentElement.offsetHeight : 600;
         this.ctx.imageSmoothingEnabled = config.antialias?.enabled ?? true;
@@ -34,6 +40,7 @@ export class Renderer {
             height: ${config.adjust ? '100%' : (config.height ?? 800) + 'px' };
         `;
 
+        // Setup resize canvas on window resize
         if (config.resizeTrack) {
             window.addEventListener('resize', () => {
                 if (config.dependsElement) {
@@ -54,15 +61,22 @@ export class Renderer {
             height: window.innerHeight,
             dependsElement: window,
             resizeTrack: true,
-            style: `
-                position: fixed;
-                z-index: 10;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-            `,
+            style: `position: fixed;z-index: 10;top: 0;left: 0;right: 0;bottom: 0;`,
             fps: 60
         });
+    }
+
+    /** Event listeners getter */
+    public get events(): {on, once, off} {
+        return {
+            on: this.obersvable.on,
+            once: this.obersvable.once,
+            off: this.obersvable.off
+        };
+    }
+
+    /** Set or disable fps limiter */
+    public set fpsLimit(val: number | false) {
+        this.fps = val;
     }
 }

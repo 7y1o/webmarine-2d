8336b43a6332aebf6a1d7907f51a25253132c5de
7y1o <tdesu.webdesign@gmail.com>
2021-07-31 23:29:18 +0300

    Created parameters in Camera

--- src/renderer/Camera.ts
+++ src/renderer/Camera.ts
@@ -1,2 +1,27 @@
+import { Vector2 } from "../core/Vector2";
+import { Filter } from "./Filter";
+import { RenderTarget } from "./RenderTarget";
+
 /** Camera class */
-export class Camera {};
+export class Camera {
+    
+    /** Position vector */
+    protected _position: Vector2;
+
+    /** Scale (simulate Z-dim) */
+    protected _scale: Vector2;
+
+    /** Camera rotation (in degrees) */
+    protected _rotation: number;
+
+    /** Tracking object instance */
+    protected _tracking: RenderTarget;
+
+    /** Camera filters */
+    protected _filters: Filter[];
+
+    /** Make camera */
+    public constructor() {
+        
+    }
+};

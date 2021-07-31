4c249b822bf6f87e14124b2579c9646d6f6245f6
7y1o <tdesu.webdesign@gmail.com>
2021-07-31 23:25:42 +0300

    Added renderer, constructor, start, pause and stop methods

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -1,8 +1,32 @@
+import { EngineConfig } from "./refs/EngineConfig";
 import { Renderer } from "./Renderer";
 
 /** Entry point of game engine */
 export class Engine {
 
     /** Renderer class holder */
-    protected render: Renderer;
+    protected renderer: Renderer;
+
+    /** Engine constructor */
+    public constructor(config?: EngineConfig) {
+        if (!config) config = {};
+
+        this.renderer = new Renderer(config.renderer ?? {});
+    }
+
+    /** Start game */
+    public start(): void {
+        this.renderer.start();
+    }
+
+    /** Pause game */
+    public pause(): void {
+        this.renderer.running = false;
+    }
+
+    /** Stop game */
+    public stop(): void {
+        this.renderer.running = false;
+        this.renderer.clear();
+    }
 }

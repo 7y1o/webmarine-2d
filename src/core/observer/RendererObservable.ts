439032794c54928e6710232b19bbcd71fc001e3e
7y1o <tdesu.webdesign@gmail.com>
2021-08-02 13:15:42 +0300

    Changed RendererObservable class type to non-abstract

--- src/core/observer/RendererObservable.ts
+++ src/core/observer/RendererObservable.ts
@@ -2,17 +2,11 @@ import { Observable } from "./Observable";
 import { Observer } from "./Observer";
 
 /** Renderer observable */
-export abstract class RendererObservable extends Observable {
+export class RendererObservable extends Observable {
     
     /** Observers */
     protected listeners: { event?: Observer[], before?: Observer[], after?: Observer[] };
 
     /** Event types */
     protected events: 'before' | 'after';
-
-    public abstract on(event: typeof this.events, callback: () => void): number;
-    public abstract once(event: typeof this.events, callback: () => void): number;
-    public abstract off(event: typeof this.events, id: number): void;
-    public abstract emit(event: typeof this.events): void;
-    public abstract clear(event?: typeof this.events): number;
 }

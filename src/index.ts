ab3888ea9c40459b8c60c2f9c09342b82fb8d8d0
7y1o <tdesu.webdesign@gmail.com>
2021-08-05 14:17:29 +0300

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import { Vector1 } from "./core/vector/Vector1";
 import { Vector2 } from "./core/vector/Vector2";
 import { Observable } from "./core/observer/Observable";
 
+import { Renderer } from "./render/Renderer";
+import { RenderScene } from "./render/RenderScene";
+import { RenderTarget } from "./render/RenderTarget";
+
 // Exports
 export default {
-    core: { Vector1, Vector2, Observable }
+    Core: { Vector1, Vector2, Observable },
+    Render: { Renderer, RenderScene, RenderTarget }
 };

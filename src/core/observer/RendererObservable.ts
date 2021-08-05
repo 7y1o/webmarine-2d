95fa78fcb3550598ce748f18e1173d672a7ab862
7y1o <tdesu.webdesign@gmail.com>
2021-08-05 17:17:53 +0300

    Removed event types, added super call

--- src/core/observer/RendererObservable.ts
+++ src/core/observer/RendererObservable.ts
@@ -7,6 +7,8 @@ export class RendererObservable extends Observable {
     /** Observers */
     protected listeners: { event?: Observer[], before?: Observer[], after?: Observer[] };
 
-    /** Event types */
-    protected events: 'before' | 'after';
+    /** Make render engine observable */
+    public constructor() {
+        super();
+    }
 }

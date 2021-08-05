3f366e1edf37c7d862595fbf768325680013aaeb
7y1o <tdesu.webdesign@gmail.com>
2021-08-05 15:23:04 +0300

    Finished renderer scene class

--- src/render/RenderScene.ts
+++ src/render/RenderScene.ts
@@ -1,10 +1,26 @@
 /** Renderer scene */
-import {RenderTarget} from "./RenderTarget";
+import { RenderCamera } from "./RenderCamera";
+import { RenderTarget } from "./RenderTarget";
 
 export class RenderScene {
 
     /** Array of items on the scene */
-    private children: RenderTarget[];
+    private _children: RenderTarget[];
 
-    /**  */
+    /** Current working camera */
+    private camera: RenderCamera;
+
+    /** Render targets getter */
+    public get children(): RenderTarget[] {
+        return this._children;
+    }
+
+    /** Render scene */
+    public render(ctx: CanvasRenderingContext2D): void {
+        this.camera.transform(ctx);
+
+        for (const child of this._children) {
+            child.render(ctx);
+        }
+    }
 }

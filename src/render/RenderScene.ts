e209c82d73243423d6fdfe38536ac9a42fbe7627
7y1o <tdesu.webdesign@gmail.com>
2021-08-05 16:00:18 +0300

    Finished renderer scene class (2.0 ;D)

--- src/render/RenderScene.ts
+++ src/render/RenderScene.ts
@@ -1,4 +1,5 @@
 /** Renderer scene */
+import { CameraNotFoundError } from "../core/errors/CameraNotFoundError";
 import { RenderCamera } from "./RenderCamera";
 import { RenderTarget } from "./RenderTarget";
 
@@ -8,16 +9,35 @@ export class RenderScene {
     private _children: RenderTarget[];
 
     /** Current working camera */
-    private camera: RenderCamera;
+    private _camera: RenderCamera;
+
+    /** Make scene */
+    public constructor() {
+        this._children = [];
+    }
 
     /** Render targets getter */
     public get children(): RenderTarget[] {
         return this._children;
     }
 
+    /** Camera getter */
+    public get camera(): RenderCamera {
+        return this._camera;
+    }
+
+    /** Camera setter */
+    public set camera(val: RenderCamera) {
+        this._camera = val;
+    }
+
     /** Render scene */
     public render(ctx: CanvasRenderingContext2D): void {
-        this.camera.transform(ctx);
+        if (!this._camera) {
+            throw new CameraNotFoundError();
+        }
+
+        this._camera.transform(ctx);
 
         for (const child of this._children) {
             child.render(ctx);
